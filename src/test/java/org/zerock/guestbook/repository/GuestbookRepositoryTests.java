afc392037c39b7bda684a435366b3953e7b5d553
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-02 23:39:49 +0900

    p146 단일 항목 검색 테스트

--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -42,6 +49,28 @@ public class GuestbookRepositoryTests {
 
             guestbookRepository.save(guestbook);
         }
+    }
+
+    @Test
+    public void testQuery1() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+
+        builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
     }
 
 }
